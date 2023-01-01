5d835bf2ec603a8821fc4e364486a2e49ed2218d
Evan Zhao <zevany16@gmail.com>
2023-01-01 11:28:22 -0800

    Firebase Initial Config - Created application on firebase and imported auth SDK from firebase toolkit into a new firebase.js config file - Will be using google third party login from auth SDK to allow firebase to handle most of the intermediary issues

--- recipal-client/src/pages/SignInPage.js
+++ recipal-client/src/pages/SignInPage.js
@@ -1,11 +1,11 @@
 import React from 'react';
-import iphone from '../assets/images/iphone14.png';
+import iPhone from '../assets/images/iphone14.png';
 // Might need a sticky header and footer for this
 
 export default function SignInPage() {
   return (
     <div className='constraint'>
-        <img className='iPhone' src={iphone} alt="go away error"/>
+        <img className='iPhone' src={iPhone} alt="go away error"/>
     </div>
   )
 }
---
+++ recipal-client/src/server-side/firebase.js
@@ -0,0 +1,29 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import {
+    getAuth,
+    connectAuthEmulator
+} from "firebase/auth"
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyB9OmGYB5dGWmo2y9nJKFIGRh30T_tvLqE",
+  authDomain: "recipal-fdce6.firebaseapp.com",
+  projectId: "recipal-fdce6",
+  storageBucket: "recipal-fdce6.appspot.com",
+  messagingSenderId: "374074873622",
+  appId: "1:374074873622:web:9a46fedcc2652801cda237",
+  measurementId: "G-NW0RM02KJ2"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+// Initialize Firebase Authentication and get a reference to the service
+export const auth = getAuth(app)
+
+//Allows me to run Authentication service locally for testing purposes (super fast)
+connectAuthEmulator(auth, "http://localhost:9099")
