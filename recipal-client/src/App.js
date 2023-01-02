95aec9c5d25e5abff3a7334f5200cd7d20d100ca
Evan Zhao <zevany16@gmail.com>
2023-01-01 18:56:07 -0800

    Added Google Sign-In Functionality + Minor Styling Issues - Created an Authorization context directory and file that stores user info using react's context API - Connected auth context to the sign-in page and integrated into the google button - Styling issue: Styled Google button to be above all other elements because button is unclickable otherwise - Naive Summary: Added button that redirects user to a google sign-in page that signs them in using their gmail. - To be added: sign out function, actual user authentication process, redirecting

--- recipal-client/src/App.js
+++ recipal-client/src/App.js
@@ -1,14 +1,17 @@
 import SignInPage from './pages/SignInPage';
 import HomePage from './pages/HomePage';
 import { Route, Routes } from 'react-router-dom';
+import { AuthContextProvider } from './context/AuthContext';
 
 function App() {
   return (
     <div>
-      <Routes>
-        <Route path='/' element={<SignInPage />}/>
-        <Route path='/home' element={<HomePage/>}/>
-      </Routes>
+      <AuthContextProvider>
+        <Routes>
+          <Route path='/' element={<SignInPage />}/>
+          <Route path='/home' element={<HomePage/>}/>
+        </Routes>
+      </AuthContextProvider>
     </div>
   );
 }
--- recipal-client/src/pages/SignInPage.js
+++ recipal-client/src/pages/SignInPage.js
@@ -1,16 +1,31 @@
 import React from 'react';
-import iPhone from '../assets/images/iphone14.png';
 import { GoogleButton } from 'react-google-button';
+import { UserAuth } from '../context/AuthContext';
+
+
 // Might need a sticky header and footer for this
 
-export default function SignInPage() {
+const SignInPage = () => {
+
+  const {googleSignIn} = UserAuth();
+
+  const handleGoogleSignIn = async () => {
+    try{
+      await googleSignIn()
+    } catch (error) {
+      console.log(error)
+    }
+  }
+
   return (
     <div className='constraint'>
-        <img className='iPhone' src={iPhone} alt="iPhone Frame"/>
+        <img className='iPhone' src={require('../assets/images/iphone14.png')} alt="iPhone Frame"/>
         <div className='signin-wrapper'>
           <img className='logo' src={require('../assets/images/logo.png')} alt="logo"/>
-          <GoogleButton className='google-signin-button'/>
+          <GoogleButton onClick={handleGoogleSignIn} className='google-signin-button'/>
         </div>
     </div>
   )
 }
+
+export default SignInPage
