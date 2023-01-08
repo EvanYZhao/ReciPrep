8c4bd12d321d808e19595b357a2780f0fa1e4371
Evan Zhao <zevany16@gmail.com>
2023-01-07 21:32:24 -0800

    Tentative Mobile API Integration (#2)
    * Submission Form, API Connected, Results Displayed
    - Styled a simple submission form for user ingredients
    - Connected to Spoonacular API and received valid data using form input
    - Displayed results of user input on screen in JSON.stringify() form
    - Reorganized logout button and logic into its own component for better readability
    - Main Idea: Crude API integration finished (need to style but logic is mostly done)
    
    * Rename
    recipal-client -> reciprep-client

--- reciprep-client/src/pages/HomePage.js
+++ reciprep-client/src/pages/HomePage.js
@@ -1,26 +1,77 @@
-import React from 'react';
-import { UserAuth } from '../context/AuthContext';
+import React from "react";
+import { useState, useEffect } from "react";
+import { UserAuth } from "../context/AuthContext";
+import axios from "axios";
+import { api_key } from "../components/creds";
+import LogoutButton from "../components/LogoutButton";
 
 const HomePage = () => {
-  const { user, logOut } = UserAuth()
+  const { user } = UserAuth();
+  const [data, setData] = useState({});
+  const [syncing, setSync] = useState(false);
+  const [endpoint, setendpoint] = useState(``);
 
-  const handleSignOut = async () => {
-    try{
-      await logOut()
-    } catch (error){
-      console.log(error)
-    }
-  }
+  const submitHandler = (e) => {
+    e.preventDefault();
+    let ingredients = e.target[0].value.trim();
+    setendpoint(
+      `https://api.spoonacular.com/recipes/findByIngredients?ingredients=${ingredients}&number=1&ranking=1&ignorePantry=true&apiKey=${api_key}`
+    )
+    e.target.reset()
+    setSync(true);
+  };
 
-  return (
-    <div className='constraint'>
-      <img className='iPhone' src={require('../assets/images/iphone14.png')} alt="iPhone"/>
-      <div className='signout-wrapper'>
-        <h1>Welcome {user.displayName}</h1>
-        <button className="signout-button" onClick={handleSignOut}> Sign Out </button>
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        await axios
+          .get(endpoint)
+          .then((response) => {
+            setData(response.data);
+            console.log(response.data);
+          })
+          .catch((error) => {
+            console.log(error);
+          });
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    fetchData();
+    setSync(false);
+  }, [endpoint]);
+
+  if (syncing) {
+    return "Loading...";
+  } else {
+    return (
+      <div className="constraint">
+        <img
+          className="iPhone"
+          src={require("../assets/images/iphone14.png")}
+          alt="iPhone"
+        />
+        <div className="signout-wrapper">
+          <h1>Welcome {user.displayName}</h1>
+          <form onSubmit={submitHandler} className="submission-form">
+            <label>Ingredients</label>
+            <input
+              className="ingredient-input"
+              type="string"
+              name="ingredients"
+              required
+              autoComplete="off"
+            />
+            <button type="submit" className="submit-button">
+              Submit
+            </button>
+          </form>
+          <LogoutButton />
+          <div>{JSON.stringify(data[0])}</div>
+        </div>
       </div>
-    </div>
-  )
-}
+    );
+  }
+};
 
-export default HomePage
+export default HomePage;
--- reciprep-client/src/server-side/firebase.js
+++ reciprep-client/src/server-side/firebase.js
@@ -1,6 +1,6 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { getAuth } from "firebase/auth"
+import { getAuth } from "firebase/auth";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -13,7 +13,7 @@ const firebaseConfig = {
   storageBucket: "recipal-fdce6.appspot.com",
   messagingSenderId: "374074873622",
   appId: "1:374074873622:web:9a46fedcc2652801cda237",
-  measurementId: "G-NW0RM02KJ2"
+  measurementId: "G-NW0RM02KJ2",
 };
 
 // Initialize Firebase
